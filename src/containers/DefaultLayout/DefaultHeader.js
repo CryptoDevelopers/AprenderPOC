b8a9ed95e74e3cfa9cbee54cf72fb94f3e8b8a48
Victor Weiss <victorlweiss@gmail.com>
2018-08-08 22:46:27 -0400

    Add options to video footer

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -3,7 +3,7 @@ import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLin
 import PropTypes from 'prop-types';
 
 import { AppAsideToggler, AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/logo.svg'
+import logo from '../../assets/img/ost_icon.png'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 
 const propTypes = {
@@ -22,7 +22,7 @@ class DefaultHeader extends Component {
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
+          full={{ src: logo, width: 48, height: 25, alt: 'Prendi Logo' }}
           minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
--- src/views/Video/Video.js
+++ src/views/Video/Video.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Badge, Form, FormGroup, FormText, Label, Input, CardFooter, Button, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
+import { Badge, Form, Progress, FormGroup, FormText, Label, Input, CardFooter, Button, Card, CardBody, CardHeader, Col, Row, Table } from 'reactstrap';
 import YouTube from 'react-youtube';
 
 
@@ -23,15 +23,49 @@ class Video extends Component {
           </CardHeader>
           <CardBody>
               <YouTube
-                videoId="2g811Eo7K8U"
+                videoId="JPT3bFIwJYA"
                 opts={opts}
                 onReady={this._onReady}
               />
           </CardBody>
-          <CardFooter>
-            <Button type="submit" size="sm" color="primary"><i className="fa fa-question-circle"></i> I have a question!</Button>
-            <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-          </CardFooter>
+          {/* <CardFooter> */}
+            <Table hover responsive className="table-outline mb-0 d-none d-sm-table float-right">
+                  {/* <thead className="thead-light">
+                 
+                  </thead> */}
+                    <tbody>
+                  <tr>
+                    <td className="text-center">
+                      <div className="avatar">
+                        <img src={'assets/img/avatars/3.jpg'} className="img-avatar pointer" alt="admin@bootstrapmaster.com" />
+                        <span className="avatar-status badge-success"></span>
+                      </div>
+                    </td>
+                    <td>
+                      <div>Academind</div>
+                      <div className="small text-muted">
+                        August 8, 2018
+                      </div>
+                    </td>
+
+                    <td>
+                      <div className="clearfix">
+                        <div className="float-left">
+                          9,322 views
+                        </div>
+                      </div>
+                      <Progress className="progress-xs" color="success" value="100" />
+                    </td>
+                    <td className="text-center">
+                      <img src={'assets/img/ost_icon.png'} className="pointer zoomout" style={{ fontSize: 34 + 'px' }}></img>
+                    </td>
+                    <td>
+                    <Button type="submit" size="sm" color="primary"><i className="fa fa-question-circle"></i> I have a question!</Button>
+                    </td>
+                  </tr>
+                  </tbody>
+                </Table>
+          {/* </CardFooter> */}
 
         </Card>
     )
