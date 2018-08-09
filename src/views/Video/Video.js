b2f5adf177a43de6eabf91545f75efacfa5a3d72
Victor Weiss <victorlweiss@gmail.com>
2018-08-08 21:37:50 -0400

    Place vid inside card

--- src/views/Video/Video.js
+++ src/views/Video/Video.js
@@ -9,75 +9,32 @@ class Video extends Component {
   render() {
 
     const opts = {
-      height: '390',
-      width: '640',
+      height: '375',
+      width: '620',
       playerVars: { // https://developers.google.com/youtube/player_parameters
         autoplay: 0
       }
     };
 
     return (
-      <YouTube
-      videoId="2g811Eo7K8U"
-      opts={opts}
-      onReady={this._onReady}
-    />
-    )
+        <Card className="card-vid">
+          <CardHeader>
+            <strong>Lesson 1</strong>
+          </CardHeader>
+          <CardBody>
+              <YouTube
+                videoId="2g811Eo7K8U"
+                opts={opts}
+                onReady={this._onReady}
+              />
+          </CardBody>
+          <CardFooter>
+            <Button type="submit" size="sm" color="primary"><i className="fa fa-question-circle"></i> I have a question!</Button>
+            <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
+          </CardFooter>
 
-    // return (
-    //   <Card>
-    //     <CardHeader>
-    //       <strong>Video Upload</strong>
-    //     </CardHeader>
-    //     <CardBody>
-    //
-    //     <YouTube
-    //     videoId="2g811Eo7K8U"
-    //     opts={opts}
-    //     onReady={this._onReady}
-    //   />
-    //       <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
-    //       <FormGroup row>
-    //         <Col md="3">
-    //           <Label htmlFor="text-input">Video Title</Label>
-    //         </Col>
-    //         <Col xs="12" md="9">
-    //           <Input type="text" id="text-input" name="text-input" placeholder="Title" />
-    //           <FormText color="muted">Short and sweet!</FormText>
-    //         </Col>
-    //       </FormGroup>
-    //
-    //         <FormGroup row>
-    //           <Col md="3">
-    //             <Label htmlFor="textarea-input">Description</Label>
-    //           </Col>
-    //           <Col xs="12" md="9">
-    //             <Input data-role="tagsinput" type="textarea" name="textarea-input" id="textarea-input" rows="4"
-    //                    placeholder="Please describe your video!" />
-    //           </Col>
-    //         </FormGroup>
-    //
-    //         {/* TAGS - https://github.com/olahol/react-tagsinput */}
-    //
-    //         <FormGroup row hidden>
-    //           <Col md="3">
-    //             <Label className="custom-file" htmlFor="custom-file-input">Custom file input</Label>
-    //           </Col>
-    //           <Col xs="12" md="9">
-    //             <Label className="custom-file">
-    //               <Input className="custom-file" type="file" id="custom-file-input" name="file-input" />
-    //               <span className="custom-file-control"></span>
-    //             </Label>
-    //           </Col>
-    //         </FormGroup>
-    //       </Form>
-    //     </CardBody>
-    //     <CardFooter>
-    //       <Button type="submit" size="sm" color="primary"><i className="fa fa-dot-circle-o"></i> Submit</Button>
-    //       <Button type="reset" size="sm" color="danger"><i className="fa fa-ban"></i> Reset</Button>
-    //     </CardFooter>
-    //   </Card>
-    // )
+        </Card>
+    )
   }
 }
 
